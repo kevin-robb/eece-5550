094363165bc0085c485e1204ca41adb1a60b1c94
Kevin Robb <kevin.robb@alumni.ou.edu>
2021-10-21 21:10:38 -0400

    Verified calculated velocity is correct. Assuming problem is that acceleration time is not accounted for, so it doesn't travel the commanded speed the whole time.

--- mr_ws/src/control/src/control_node.py
+++ mr_ws/src/control/src/control_node.py
@@ -45,31 +45,29 @@ def yaw_from_pose(pose):
     theta = euler_from_quaternion(quaternion)[2] # (roll,pitch,yaw)
     return theta
 
-# create a homogenous affine matrix from a pose msg or components
-def make_affine(pose=None,theta=None,x=None,y=None):
-    if pose is None:
-        return np.array([[cos(theta), -sin(theta), x], [sin(theta), cos(theta), y], [0,0,1]])
-    else:
-        return make_affine(theta=yaw_from_pose(pose), x=pose.position.x, y=pose.position.y)
+# create a homogenous affine matrix from a pose's components
+def make_affine(theta=None,x=None,y=None):
+    return np.array([[cos(theta), -sin(theta), x], [sin(theta), cos(theta), y], [0,0,1]])
 
 # calculate a trajectory to drive to the goal pose
 def calculate_trajectory(cur_pose,goal_pose, T):
     # calculate \dot\Omega using inverse kinematics
-    dotOmega = logm(inv(cur_pose) @ goal_pose) / T
+    # dotOmega = (1/T)*logm(inv(cur_pose) @ goal_pose)
+    dotOmega = (1/T)*logm(np.matmul(inv(cur_pose),goal_pose))
+    print("dotOmega:\n",dotOmega)
     # extract necessary speed commands
     cmd = Twist()
-    cmd.linear.x = dotOmega[0][2] * (1.25/0.7) # tuning param
+    cmd.linear.x = dotOmega[0][2]
     cmd.angular.z = dotOmega[1][0]
     control_pub.publish(cmd)
 
 # drive to the goal pose for a certain amount of time, then halt
 def go_to_goal(goal_pose, T):
     # starting pose is origin but offset from camera's frame slightly
-    # NOTE there should be a -x offset due to this, but I couldn't find what it should be,
-    #   so the robot ends slightly further from the apriltag than it should
-    cur_pose = make_affine(theta=0,x=0,y=0)
+    # NOTE I couldn't find a spec for this but it seems to be about +7cm on x
+    cur_pose = make_affine(theta=0,x=-0.07,y=0)
     print("initial pose:\n",cur_pose)
-    print("goal pose\n",goal_pose)
+    print("goal pose:\n",goal_pose)
     start_time = time()
     calculate_trajectory(cur_pose,goal_pose, T)
     while (time()-start_time < T):
